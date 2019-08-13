c33d0d2834ea64a875830e659dd0983da9325dfa
arabian9ts <nok14.tm59@gmail.com>
2019-08-14 03:46:11 +0900

    coreml model template

--- finger-tripping/ViewController.swift
+++ finger-tripping/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func handleOnMicSound() {
+        let pixelBuffer: CVPixelBuffer? = nil
+        
+        guard let model = try? VNCoreMLModel(for: Hypre().model) else {
+            return
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request: VNRequest, error: Error?) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                print(error!)
+                return
+            }
+            let topConfidence = results[0].identifier.components(separatedBy: ", ")[0]
+            print("top confidence index is \(topConfidence)")
+        }
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer!, options: [:]).perform([request])
+    }
 }
-
