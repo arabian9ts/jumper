fd769897f16fab3d7679f84d75cef37128e878b4
arabian9ts <nok14.tm59@gmail.com>
2019-08-14 02:38:08 +0900

    init project

---
+++ finger-tripping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finger-tripping
+//
+//  Created by arabian9ts on 2019/08/14.
+//  Copyright © 2019 arabian9ts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
