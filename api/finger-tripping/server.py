b0411964dabc2ea2a0175afbd61b47bd7700907d
masahisa <masahisa0910@gmail.com>
2019-08-15 05:35:17 +0900

    update

--- api/finger-tripping/server.py
+++ api/finger-tripping/server.py
@@ -1,59 +1,52 @@
-from prediction_pb2 import *
-from route_pb2_grpc import add_TodoGatewayServicer_to_server, TodoGatewayServicer
+from grpc.prediction_pb2 import *
+from grpc.prediction_pb2_grpc import add_PredictionServiceServicer_to_server, PredictionServiceServicer
 
 from concurrent import futures
 from datetime import datetime
 import time
 import grpc
 
+import os
+import numpy as np
+
+from keras.models import load_model
+
+from config import *
+from util.mel import *
+
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+def restore_model():
+    return load_model(os.path.dirname(os.path.abspath(__file__)) + '/model/alexnet.h5')
 
-def get_timestamp():
-    return datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+class RoutePredictServicer(PredictionServiceServicer):
+    def __init__(self):
+        restore_model()
 
+    def Predict(self, request, response):
+        print("Predict Called : %s" % request.timestamp)
+        sound = np.array(request.magnitudes[:conf.sampling_rate * conf.duration]) / 32767
+        melspectrogram = sound_to_mel(conf, sound)
 
-class RouteTodoServicer(TodoGatewayServicer):
-    """
-    Todoサーバーの実装
-    protoで定義したservice名のメソッドを実装する
-    """
-
-    todos = {}
-
-    # TODOを作成するロジック
-    def TodoCreate(self, request, response):
-        # request.fieldでprotoで定義した値にアクセスできる
-        print("Todo Create Called : %s" % request.timestamp)
-        try:
-            self.todos[request.todo_name] = TodoComponent(
-                todo_name=request.todo_name,
-                is_done=False    # 作成時はTODO完了していないためFalse
-            )
-            is_success = True
-            message = None
-        except:
-            import traceback
-            traceback.print_exc()
-            is_success = False
-            message = "something happen"
+        if melspectrogram is not None:
+            reshaped = np.reshape(melspectrogram, (1, 128, 128, 1))
+            probs = model.predict(reshaped)
+            label = parse_to_label(probs)
+        else:
+            label = parse_to_label(0)
 
         # レスポンスを返す時はreturnするだけで良い
-        return TodoCreateResponse(
-            response=ServerResponseComponent(
-                is_success=is_success,
-                message=message
-            ),
-            timestamp=get_timestamp()
+        return PredictResponse(
+            label=label
         )
 
 
 # サーバーの実行
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    add_TodoGatewayServicer_to_server(
-        RouteTodoServicer(), server)
+    add_PredictionServiceServicer_to_server(
+        PredictionServiceServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     print("Server Start!!")
